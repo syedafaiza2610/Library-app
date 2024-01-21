7d9a736ab2e3da83e03a63ce9ba02f66c7d991bc
syeda Faiza <syedafaiza2610@gmail.com>
2024-01-21 13:01:37 +0500

    remov key

--- js/firebase.js
+++ js/firebase.js
@@ -6,13 +6,7 @@ import {getFirestore , collection , addDoc , query , where , getDocs , getDoc , 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
  const firebaseConfig = {
-  apiKey: "AIzaSyCE4_oje0ZknaKX1xU2-8ZMR594CBjF4oM",
-  authDomain: "smit-ba688.firebaseapp.com",
-  databaseURL: "https://smit-ba688-default-rtdb.firebaseio.com",
-  projectId: "smit-ba688",
-  storageBucket: "smit-ba688.appspot.com",
-  messagingSenderId: "1014681951996",
-  appId: "1:1014681951996:web:93bcec13ac2e8b709c4179"
+
 };
 
 
